b7d5d2d48d368463779033da9b568a02dbbeccfc
TimThuis <tim.thuis@gmail.com>
2016-04-26 15:11:18 +0200

    add

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,42 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require_tree .
+
+
+// smooth scroll
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+// popup
+window.onload = function() {
+  document.getElementById("btnCharacter").onclick = function(){
+        var overlay = document.getElementById("overlay");
+        var popup = document.getElementById("popup");
+        overlay.style.display = "block";
+        popup.style.display = "block";
+    };
+
+  document.getElementById("CloseBtn").onclick = function(){
+        var overlay = document.getElementById("overlay");
+        var popup = document.getElementById("popup");
+        overlay.style.display = "none";
+        popup.style.display = "none";
+  }
+};
+
+// viewport
+window.sr = ScrollReveal();
+sr.reveal('.foo');
+sr.reveal('.bar');
